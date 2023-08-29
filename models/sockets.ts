61c35261b18d808144d92e50e9d65ed2ee491af6
Iván Contreras <ivan.contreras@etpay.com>
2023-08-28 22:55:49 -0400

    debug

--- models/sockets.ts
+++ models/sockets.ts
@@ -34,10 +34,39 @@ export default class Sockets {
 
             socket.on('message', function message(data) {
                 const queryData = url.parse(data.toString(), true).query;
+
+                let message: any = {
+                    day: queryData.day
+                };
+
+                let isOpen = false;
+
+                if (queryData.status === "OPEN") {
+                    isOpen = true;
+                }
+                if (queryData.field === 'morning') {
+                    if (isOpen) {
+                        message.morning = 'OPEN';
+                        message.afternoon = 'CLOSED';
+                    } else {
+                        message.morning = 'CLOSED';
+                        message.afternoon = 'OPEN';
+                    }
+                }else {
+                    if (isOpen) {
+                        message.morning = 'CLOSED';
+                        message.afternoon = 'OPEN';
+                    } else {
+                        message.morning = 'OPEN';
+                        message.afternoon = 'CLOSED';
+                    }
+                }
+
+
                 const esp32Id = queryData.esp32Id as string;
                 console.log(id + ' enviando a ESP32 '+ esp32Id)
                 if (users[esp32Id]) {
-                    users[esp32Id].send(data.toString());
+                    users[esp32Id].send(message);
                 } else {
                     console.log('ESP32 '+ esp32Id + ' no conectado');
                 }
