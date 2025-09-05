aaa5c8febb98c8e8e3c758d59a1f4d950c315be7
Iván Contreras <ivan.contreras@etpay.com>
2025-09-05 11:33:57 -0400

    add: close_rest

--- controllers/statusV2.ts
+++ controllers/statusV2.ts
@@ -22,9 +22,9 @@ export const updateStatusV2 = async (req: Request, res: Response, next: NextFunc
         let date = new Date();
         const { id } = req.params;
 
-        const { key, value } = req.body;
+        const { key, value, close_rest } = req.body;
 
-        const filter = { _id: id, user: req.body.id };
+        const filter = { _id: id, user: req.body.id, };
 
         let body : any = {};
         body[key] = value;
@@ -33,6 +33,25 @@ export const updateStatusV2 = async (req: Request, res: Response, next: NextFunc
         if (key === 'enabledPM') body['updatedAtPM'] = date;
 
         await StatusV2Model.findOneAndUpdate(filter, body, { new: true });
+
+        if (close_rest) {
+            const status = await StatusV2Model.find({_id: {$ne: id}});
+            for (const s of status) {
+                let body: any = {};
+
+                if (s.enabledAM) {
+                    body['enabledAM'] = false;
+                    body['updatedAtAM'] = date;
+                }
+
+                if (s.enabledPM) {
+                    body['enabledPM'] = false;
+                    body['updatedAtPM'] = date;
+                }
+                
+                await StatusV2Model.findOneAndUpdate({_id: s._id}, body, { new: true });
+            }
+        }
 
         const respoonse = await StatusV2Model.find({ user: req.body.id });   
         
